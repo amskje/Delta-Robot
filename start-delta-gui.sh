70937dd02656ab5dd9b7f97acbc5be003f335311
amskje <amunskj@stud.ntnu.no>
2025-07-17 08:56:14 +0200

    Bootfixes

--- start-delta-gui.sh
+++ start-delta-gui.sh
@@ -7,7 +7,7 @@ sleep 3
 docker run -it --rm --net=host \
   -e DISPLAY=:0 \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -v /home/delta/Delta-Robot/RaspberryPi:/home/dev/Delta-Robot \
+  -v ~/Delta-Robot/RaspberryPi:~/Delta-Robot \
   -e ROS_VERSION=2 \
   -e ROS_DISTRO=humble \
   -e ROS_DOMAIN_ID=0 \
