28c1660550988ecd3731d1af79d628bea7dd8eb2
Haavard <haavard.akselsen@nov.com>
2025-08-07 14:36:55 +0200

    test

--- RaspberryPi/GUIMultiplePages.py
+++ RaspberryPi/GUIMultiplePages.py
@@ -428,7 +428,9 @@ if __name__ == "__main__":
     app = App()
 
     twist_publisher.register_handler("SETUP_FINISHED", lambda: app.after(0, lambda: app.show_frame(StartScreen)))
-    twist_publisher.register_handler("REBOOT", lambda: app.after(0, lambda: reboot_app(app)))
+    twist_publisher.register_handler("REBOOT", lambda: app.after(0, lambda: app.show_frame(ManualScreen)))
+
+    #twist_publisher.register_handler("REBOOT", lambda: app.after(0, lambda: reboot_app(app)))
 
 
 
